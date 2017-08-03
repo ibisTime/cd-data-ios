6699bac531d4766200d4d841029d847416a32d66
czyGod <a549102100@163.com>
2017-08-03 13:25:12 +0800

    添加认证结果

---
+++ Base_iOS/Classes/Modules/数据时代/Controller/AuthResult/ZMFoucsResultVC.h
@@ -0,0 +1,13 @@
+//
+//  ZMFoucsResultVC.h
+//  Base_iOS
+//
+//  Created by 蔡卓越 on 2017/8/1.
+//  Copyright © 2017年 caizhuoyue. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ZMFoucsResultVC : BaseViewController
+
+@end
---
+++ Base_iOS/Classes/Modules/数据时代/Controller/AuthResult/ZMOPScoreResultVC.h
@@ -0,0 +1,13 @@
+//
+//  ZMOPScoreResultVC.h
+//  Base_iOS
+//
+//  Created by 蔡卓越 on 2017/8/1.
+//  Copyright © 2017年 caizhuoyue. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface ZMOPScoreResultVC : BaseViewController
+
+@end
